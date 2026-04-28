d5b543cc687b8ac38b8557e8a3540635628d7e35
Eduardo Esteves <eduesteves@gmail.com>
2026-04-28 16:15:50 -0300

    main.py 2

--- main.py
+++ main.py
@@ -3,17 +3,12 @@ from services.observatorio import executar_rota
 # Para usar o código, basta retirar o # do comando desejado nas linhas abaixo e rodar o arquivo main.py.
 
 # Testando a rota de cadastro de projetos com dados válidos e inválidos
-#print(executar_rota("/projetos", "POST", {"titulo": "Hub Cultural", "curso": "ADS", "periodo": "2024.1", "descricao": "Projeto de integração para o curso de Análise e Desenvolvimento de Sistemas."})) 
-# print(executar_rota("/projetos", "POST", {"titulo": "", "curso": "", "periodo": "", "descricao": ""})) 
+#print(executar_rota("/projetos", "POST", {"titulo": "Hub Cultural", "curso": "ADS", "periodo": "2024.1", "descricao": "Projeto de integração para o curso de Análise e Desenvolvimento de Sistemas."}))
+#print(executar_rota("/projetos", "POST", {"titulo": "Olhos abertos", "curso": "ADS", "periodo": "2024.1", "descricao": "Outro projeto de integração para o curso de Análise e Desenvolvimento de Sistemas."}))
+#print(executar_rota("/projetos", "POST", {"titulo": "", "curso": "", "periodo": "", "descricao": ""})) 
 
-# Testando se quando um projeto é cadastrado, ele aparece na lista de projetos
+# Testando se quando projetos são cadastrados, eles aparecem na lista de projetos
 # print(executar_rota("/projetos", "GET"))
-
-#Adicionando mais um projeto para testar a busca por nome e filtro por curso
-#print(executar_rota("/projetos", "POST", {"titulo": "Olhos abertos", "curso": "ADS", "periodo": "2024.1", "descricao": "Projeto de integração para o curso de Análise e Desenvolvimento de Sistemas."}))
-
-# Testando se o novo projeto foi adicionado e aparece na lista de projetos
-#print(executar_rota("/projetos", "GET"))
 
 # Testando a busca por nome de projeto
 #print(executar_rota("/projetos/buscar", "GET", {"titulo": "Olhos abertos"}))
